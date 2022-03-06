9baddef49e79d4df6f438e324ceda99973949740
Adao <adaojreess@gmail.com>
2022-03-06 19:31:23 -0300

    refactor

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,11 +1,29 @@
-const { queryString } = require("./queryString");
-describe("Object to query string", () => {
-  it("should crete a valid query string when an object provided", () => {
+const { queryString } = require('./queryString');
+describe('Object to query string', () => {
+  it('should crete a valid query string when an object provided', () => {
     const obj = {
-      name: "Adão",
-      profession: "Developer",
+      name: 'Adão',
+      profession: 'Developer',
     };
-    expect(queryString(obj)).toBe("name=Adão&profession=Developer");
+    expect(queryString(obj)).toBe('name=Adão&profession=Developer');
+  });
+
+  it('should create a valid query string even when an array is passed as value', () => {
+    const obj = {
+      name: 'Adão',
+      abilities: ['js', 'ts', 'tdd'],
+    };
+    expect(queryString(obj)).toBe('name=Adão&abilities=js,ts,tdd');
+  });
+
+  it('should throw an error when an object is passed as object', () => {
+    const obj = {
+      name: 'Adão',
+      abilities: { first: 'js', second: 'ts' },
+    };
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
   });
 });
 

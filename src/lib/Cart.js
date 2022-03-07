01f29692cac6ee01b51b7ef2a3388d1cff9aed48
Adao <adaojreess@gmail.com>
2022-03-06 23:45:22 -0300

    Shopping Cart: Assegurar que um produto só apareça uma vez no carrinho + Lodash

--- src/lib/Cart.js
+++ src/lib/Cart.js
@@ -1,13 +1,19 @@
+import find from 'lodash/find';
+import remove from 'lodash/remove';
 export default class Cart {
   items = [];
 
-  add = item => {
+  add(item) {
+    const itemToFind = { product: item.product };
+    if (find(this.items, itemToFind)) {
+      remove(this.items, itemToFind);
+    }
     this.items.push(item);
-  };
+  }
 
-  getTotal = () => {
+  getTotal() {
     return this.items.reduce((acc, item) => {
       return acc + item.product.price * item.quantity;
     }, 0);
-  };
+  }
 }
--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -1,9 +1,14 @@
 import Cart from './Cart';
 describe('Cart', () => {
   let cart;
+  let product;
 
   beforeEach(() => {
     cart = new Cart();
+    product = {
+      title: 'Adidas running shoes | men',
+      price: 35388, // 353.88 || R$ 353,88
+    };
   });
 
   it('should return 0 when getTotal() is executed in a newly created instance', () => {
@@ -12,14 +17,25 @@ describe('Cart', () => {
 
   it('should multiple quantity and price ans receive the total amount', () => {
     const item = {
-      product: {
-        title: 'Adidas running shoes | men',
-        price: 35388, // 353.88 || R$ 353,88
-      },
-      quantity: 2, // 70776
+      product,
+      quantity: 2,
     };
     cart.add(item);
 
     expect(cart.getTotal()).toEqual(70776);
+  });
+
+  it('should ensure no more than on product exists at a time', () => {
+    cart.add({
+      product,
+      quantity: 2,
+    });
+
+    cart.add({
+      product,
+      quantity: 1,
+    });
+
+    expect(cart.getTotal()).toEqual(35388);
   });
 });
